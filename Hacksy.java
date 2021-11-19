fe8e4e12083882a0b91c33b2d4f937006db259ea
Roshan Rao <89931020+roshanarao@users.noreply.github.com>
2021-11-19 10:04:24 -0500

    Add files via upload

---
+++ Hacksy.java
@@ -0,0 +1,17 @@
+public class Hacksy {
+    int [] solutions;
+    int row = 1;
+    int col = 2;
+
+    public Hacksy(){
+        Labyrinth labby = new Labyrinth(row, col);
+        
+
+
+    }
+
+    
+
+
+    
+}
