71a27f045ca1eeb6ff7ff3c0dd347be4ea99c982
Roshan Rao <rrao@friendsbalt.org>
2021-12-02 21:23:33 -0500

    recurse, dammit

--- Hacksy.java
+++ Hacksy.java
@@ -3,12 +3,36 @@ public class Hacksy {
     int row = 1;
     int col = 2;
 
-    public Hacksy(){
-        Labyrinth labby = new Labyrinth(row, col);
+
+    public Hacksy(Labyrinth l){
+        
+    }
+
+    public int[] isSafeMove(Labyrinth l){
+       for(int i = 0; i < row; i++){
+           for(int j = 0; j < col; j++){
+               l.isStone(i, j);
+               l.isValid(i, j);
+           }
+       }
+       
+       
+       
+        return solutions; 
+    }
+    
+
+    public static void main(String[] args){
+        Labyrinth test = new Labyrinth(10, 10);
+        Hacksy simulation = new Hacksy(test);
+        test.printGrid();
+        //simulation.isSafeMove(test);
+        
+    }
         
 
 
-    }
+    
 
     
 
